aa32c5821f2b9de928f8a0813ddfc1f43101c0fa
Eliza Taylor <elizataylor2002@gmail.com>
2018-07-16 13:35:31 +0000

    automatically backed up by learn

--- amazon_transaction.rb
+++ amazon_transaction.rb
@@ -2,12 +2,16 @@
 class AmazonTransaction
   attr_accessor :total, :item, :discount
   
-  def initialize(discount)
+  def initialize(discount + 20)
     @total = 0 
     @item = []
     @discount = discount 
-  
-  
 end 
 
+def add_item(title, price)
+  
+  @total += price
+  @items << title 
+  end 
 end 
+
